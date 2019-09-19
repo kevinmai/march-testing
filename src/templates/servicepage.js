92ed64bdc83f09d3f09ce12b625c811cecc3350b
PIeter Tenret <pietertenret@gmail.com>
2019-09-19 14:06:06 -0500

    Update OurServices Variable

--- src/templates/servicepage.js
+++ src/templates/servicepage.js
@@ -125,7 +125,7 @@ export default ({ data }) => (
                             <h2>Our Services</h2>
                             <hr style={{ backgroundColor: data.sanityCompanyInfo.accentcolor.hex }} />
                             <BlockContent blocks={data.sanityPages._rawServices} />
-                            <a href={"/our-services?city=" + city} style={{ backgroundColor: data.sanityCompanyInfo.accentcolor.hex }}>View our Services</a>
+                            <a href={ourServices} style={{ backgroundColor: data.sanityCompanyInfo.accentcolor.hex }}>View our Services</a>
                         </div>
                     </div>
                     <div className="container pageContent">
