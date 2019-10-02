78d2844e6dd7025d889eaab2e016ab225bcf4a18
Pieter <pieter.tenret@vitalstorm.com>
2019-10-02 15:08:39 -0500

    remove timeout

--- src/templates/ourServices.js
+++ src/templates/ourServices.js
@@ -138,15 +138,13 @@ export default ({ data }) => (
             <script>{`
                 /* REPLACE COMPANYNAME IN COPY */
                 if(typeof window !== 'undefined'){
-                    setTimeout(
-                            $(function(){
-                                $(".firstCopy p").each(function(){
-                                    var text = $(this).text();
-                                    text = text.replace("companyname", "${data.sanityCompanyInfo.companyname}");
-                                    $(this).text(text);
-                                });
-                            })
-                        ,1000)
+                    $(function(){
+                        $(".firstCopy p").each(function(){
+                            var text = $(this).text();
+                            text = text.replace("companyname", "${data.sanityCompanyInfo.companyname}");
+                            $(this).text(text);
+                        });
+                    });
                 }
             `}</script>
         </Helmet>
