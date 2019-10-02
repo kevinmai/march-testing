28f9ef6bc9d595a6adc9fd32026e6be4ebd535f2
Pieter <pieter.tenret@vitalstorm.com>
2019-10-02 16:46:36 -0500

    window load instead of document ready

--- src/templates/ourServices.js
+++ src/templates/ourServices.js
@@ -137,7 +137,7 @@ export default ({ data }) => (
             <script>{`
                 /* REPLACE COMPANYNAME IN COPY */
                 if(typeof window !== 'undefined'){
-                    $(document).ready(function(){
+                    $(window).load(function(){
                         $(".firstCopy p").each(function(){
                             var text = $(this).text();
                             text = text.replace("companyname", "${data.sanityCompanyInfo.companyname}");
