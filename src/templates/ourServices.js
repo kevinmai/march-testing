07a72d446d0c35c302cfb307f18c66ebb47220df
Pieter <pieter.tenret@vitalstorm.com>
2019-10-03 10:21:12 -0500

    add async react helmet

--- src/templates/ourServices.js
+++ src/templates/ourServices.js
@@ -2,7 +2,7 @@
 import  React from 'react'
 import { graphql } from 'gatsby'
 import Layout from "../components/layout"
-import Helmet from 'react-helmet'
+import Helmet from 'react-helmet-async'
 import { Link } from "gatsby"
 import BlockContent from '../components/block-content'
 import BackgroundImage from 'gatsby-background-image'
