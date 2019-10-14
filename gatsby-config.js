36222bd325cc5b33823a01c1e71c31bf161f5cec
Pieter <pieter.tenret@vitalstorm.com>
2019-10-14 16:00:21 -0500

    no manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,18 +26,18 @@ module.exports = {
         token: 'sktnCRevILcYrRD3kqsDwrmmyfs5i2wSUcLBQNCgtB56huJORRPPbgAc1IGozgfov356BMJDvZh1j76KRwG0J06WtRG6UVm9B85Rad5kRw7K16p8ETmjKMmlRrcR6EO2Bu1vzsDb42OlnkvTLe82rVXoeFkvvcaVuGClqz3T7NXnJzLArVnU'
       }
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        //background_color: `#663399`,
-        //theme_color: `#663399`,
-        display: `standalone`,
-        //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     //background_color: `#663399`,
+    //     //theme_color: `#663399`,
+    //     display: `standalone`,
+    //     //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
   `gatsby-plugin-styled-components`,
   `gatsby-plugin-netlify`
   ],
