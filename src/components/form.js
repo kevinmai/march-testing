e59714312264718adc61351142ce765bbd3c6dd3
Pieter <pieter.tenret@vitalstorm.com>
2020-02-13 15:15:07 -0600

    Netfify form test

--- src/components/form.js
+++ src/components/form.js
@@ -29,15 +29,16 @@ export default class ContactForm extends React.Component {
     $('.inputfield').val("");
 
   }
+  
    render() {
        
     return (
       <div className="form">
         <h2>Schedule Service</h2>  
         <span className="closeForm" onClick={this.changeActive}><FaTimesCircle /></span>
-      <form id="form-metrics" validate onSubmit={this.handleSubmit} action="https://metrics.vitalstorm.com/email_form_submission/MGYyZmE4Zjc2N2UwNTY2NzNkMzEwYzYyMjU4NTFkNTk/" method="post">
+      <form id="form-metrics" validate onSubmit={this.handleSubmit} action="https://metrics.vitalstorm.com/email_form_submission/MGYyZmE4Zjc2N2UwNTY2NzNkMzEwYzYyMjU4NTFkNTk/" method="POST" data-netlify="true">
         <input id="mail-name" className="inputfield" type="text" name="name" placeholder="Enter your full name" required />
-        <input id="mail-email" className="inputfield" type="text" name="honeypot" placeholder="Email address" minlength="3" maxlength="64" required />
+        <input id="mail-email" className="inputfield" type="text" name="honeypot" placeholder="Email address" minLength="3" maxLength="64" required />
         <input id="mail-honey" className="inputfield" type="text" name="email" />
         <input id="mail-tel" className="inputfield" name="tel" type="tel" placeholder="(123) 456-7890" required />
         <input id="mail-message" className="inputfield" type="text" name="message" placeholder="Request a service" required />
