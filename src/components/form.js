aff2b34dff955672fb9c4a99b17358d4fec84f1e
Pieter <pieter.tenret@vitalstorm.com>
2020-11-23 10:24:30 -0600

    fix form submission functionality

--- src/components/form.js
+++ src/components/form.js
@@ -1,22 +1,45 @@
 import React from "react"
 import $ from "jquery"
-import { FaTimesCircle, FaEnvelope, FaPhone } from 'react-icons/fa'
 import { StaticQuery, graphql } from "gatsby"
-
+import { FaTimesCircle, FaEnvelope, FaPhone } from 'react-icons/fa'
 import Image from "gatsby-image"
 
+const FormMessage = props => {
+  let message = 'Thank you for your submission, we will be with you shortly.';
+  let style = {};
+  if(props.submitted){
+    message = 'Thank you for your submission, we will be with you shortly.';
+    style.display = 'block';
+  }
+  return (
+    <p className="form-message" style={style}>
+      {message}
+      <FaEnvelope />
+    </p>
+  );
+};
 
 
 export default class ContactForm extends React.Component {
-    
-  state = {
-    name: "",
-    honeypot: "",
-    tel: "",
-    message: "",
-  }
+  constructor(props) {
+    super(props);
+    this.formRef = React.createRef();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.changeActive = this.changeActive.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
-  handleInputChange = event => {
+    this.state = {
+      name: "",
+      honeypot: "",
+      tel: "",
+      message: "",
+      // vsref: "",
+      gclid: "",
+      submitted: false,
+      formAction: 'https://metrics.vitalstorm.com/email_form_submission/OTkwMjkyMjdhYThjZTBmMGM0NDNkM2Y2YzE2MWEzMjU/'
+    };
+  }
+  handleInputChange(event){
     const target = event.target
     const value = target.value
     const name = target.name
@@ -25,31 +48,38 @@ export default class ContactForm extends React.Component {
       [name]: value,
     })
   }
-
-  changeActive = event =>{
+  changeActive(event){
     $(".form").toggleClass("expanded");
-    $('body').toggleClass("formExpanded");
+    $("body").toggleClass("formExpanded");
+    console.log("expanded");
+
   }
-
-  handleSubmit = event => {
+  handleSubmit(event){
     event.preventDefault();
-    
-
-    var form = $("#form-metrics");
-    var url = form.attr('action');
-
+    const fieldData = new URLSearchParams({
+      name: this.state.name,
+      honeypot: this.state.honeypot,
+      tel: this.state.tel,
+      message: this.state.message,
+      vsref: this.state.vsref,
+      gclid: this.state.gclid,
+    });
     $.ajax({
       type: "POST",
-           url: url,
-           data: form.serialize(), // serializes the form's elements.
-           success: function(data)
-           {
-               //alert(data); // show response from the php script.
-           }
+       url: this.state.formAction,
+       data:  fieldData.toString(), // serializes the form's elements.
+       success: data => {
+          this.setState({
+            name: "",
+            honeypot: "",
+            tel: "",
+            message: "",
+            vsref: "",
+            gclid: "",
+            submitted: true
+          });
+       }
     });
-
-    $(".form-message").text('Thank you for your submission, we will be with you shortly.').css('display', 'block');
-    $('.inputfield').val("");
   }
   
  
@@ -107,19 +137,19 @@ export default class ContactForm extends React.Component {
                             <h2>Schedule Service</h2>  
                             <p>Fill out the form below and we'll reach out to schedule your service appointment. </p>
                             <span className="closeForm" onClick={this.changeActive} style={{fill: data.sanityCompanyInfo.primarycolor.hex}}><FaTimesCircle /></span>
-                              <form id="form-metrics" onSubmit={this.handleSubmit} action={"https://metrics.vitalstorm.com/email_form_submission/" + data.sanityCompanyInfo.formhash} method="POST">
-                                <input id="mail-name" className="inputfield" type="text" name="name" value={this.state.name} onChange={this.handleInputChange} placeholder="Enter your full name" required />
-                                <input id="mail-email" className="inputfield" type="text" name="honeypot" value={this.state.honeypot} onChange={this.handleInputChange} placeholder="Email address" minLength="3" maxLength="64" required />
-                                <input id="mail-honey" className="inputfield" type="text" name="email" />
-                                <input id="mail-tel" className="inputfield" name="tel" value={this.state.tel} type="tel" onChange={this.handleInputChange} placeholder="(123) 456-7890" required />
-                                <input id="mail-message" className="inputfield" type="text" value={this.state.message} onChange={this.handleInputChange} name="message" placeholder="Request a service" required />
-                                <input type="hidden" name="gclid" value="" />
-                                <input type="hidden" name="vsref" value="6028420262" />
-                                <div className="ajax-button">
-                                    <button id="mail-submit" type="submit" name="mail-submit" style={{backgroundColor: data.sanityCompanyInfo.primarycolor.hex}} >Send Request</button> 
-                                </div>
-                                <p className="form-message"><FaEnvelope /> </p>
-                                </form>
+                            <form ref={this.formRef} id="form-metrics" onSubmit={this.handleSubmit} action={"https://metrics.vitalstorm.com/email_form_submission/" + data.sanityCompanyInfo.formhash} method="POST">
+                              <input id="mail-name" className="inputfield" type="text" name="name" value={this.state.name} onChange={this.handleInputChange} placeholder="Enter your full name" required />
+                              <input id="mail-email" className="inputfield" type="text" name="honeypot" value={this.state.honeypot} onChange={this.handleInputChange} placeholder="Email address" minLength="3" maxLength="64" required />
+                              <input id="mail-honey" className="inputfield" type="text" name="email" />
+                              <input id="mail-tel" className="inputfield" name="tel" value={this.state.tel} type="tel" onChange={this.handleInputChange} placeholder="(123) 456-7890" required />
+                              <input id="mail-message" className="inputfield" type="text" value={this.state.message} onChange={this.handleInputChange} name="message" placeholder="Request a service" required />
+                              <input type="hidden" name="gclid" value="" />
+                              <input type="hidden" name="vsref" value="" />
+                              <div className="ajax-button">
+                                  <button id="mail-submit" type="submit" name="mail-submit" style={{backgroundColor: data.sanityCompanyInfo.primarycolor.hex}}>Send Request</button>
+                              </div>
+                              <FormMessage submitted={this.state.submitted} />
+                            </form>
                             </div>
                         </div>
                       </div>
